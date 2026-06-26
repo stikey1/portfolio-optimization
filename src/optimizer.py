e08ed51588848613ef97fb7830ac0d3078ae35f2
stikey1 <stikey123@gmail.com>
2026-06-26 14:24:50 -0500

    commented out optimizer

--- src/optimizer.py
+++ src/optimizer.py
@@ -5,14 +5,14 @@
 # from scipy.optimize import minimize
 
 
-def portfolio_return(weights: np.ndarray, expected_returns: np.ndarray) -> float:
-    return 0.0
+# def portfolio_return(weights: np.ndarray, expected_returns: np.ndarray) -> float:
+#     return 0.0
 
-def portfolio_variance(weights: np.ndarray, cov_matrix: np.ndarray) -> float:
+# def portfolio_variance(weights: np.ndarray, cov_matrix: np.ndarray) -> float:
 
-    return 0.0
+#     return 0.0
 
-def sharpe_ratio(weights : np.ndarray, expected_returns: np.ndarray, cov_matrix: np.ndarray, risk_free_rate: float = 0.0) -> float:
-    return 0.0
-def maximize_sharpe_ratio(expected_returns: np.ndarray, cov_matrix: np.ndarray, risk_free_rate: float = 0.0, asset_names: list[str] = None) -> np.ndarray:
-    return 0.0
+# def sharpe_ratio(weights : np.ndarray, expected_returns: np.ndarray, cov_matrix: np.ndarray, risk_free_rate: float = 0.0) -> float:
+#     return 0.0
+# def maximize_sharpe_ratio(expected_returns: np.ndarray, cov_matrix: np.ndarray, risk_free_rate: float = 0.0, asset_names: list[str] = None) -> np.ndarray:
+#     return 0.0
